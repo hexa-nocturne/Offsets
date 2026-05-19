749737738e9f39967164f56e25f6756823856a01
hexa-nocturne <hexanocturne@gmail.com>
2026-05-19 08:05:33 +0800

    Add files via upload

--- animationsystem_dll.zig
+++ animationsystem_dll.zig
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-05-14 22:44:29.399715100 UTC
+// 2026-05-19 00:03:10.224984900 UTC
 
 pub const cs2_dumper = struct {
     pub const schemas = struct {
--- scenesystem_dll.cs
+++ scenesystem_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-05-14 22:44:29.399715100 UTC
+// 2026-05-19 00:03:10.224984900 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: scenesystem.dll
