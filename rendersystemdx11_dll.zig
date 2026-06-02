b39d8135cee802b4a8c4c588b0f9639f22b2e8e0
hexa-nocturne <hexanocturne@gmail.com>
2026-06-02 11:29:14 +0800

    Add files via upload

--- rendersystemdx11_dll.zig
+++ rendersystemdx11_dll.zig
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-05-30 02:00:17.018674100 UTC
+// 2026-06-02 03:27:25.495108600 UTC
 
 pub const cs2_dumper = struct {
     pub const schemas = struct {
--- server_dll.cs
+++ server_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-05-30 02:00:17.018674100 UTC
+// 2026-06-02 03:27:25.495108600 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: server.dll
