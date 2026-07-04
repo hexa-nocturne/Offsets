6da72459e8647b8fa08c8097875782ef5ed0720a
Hexa <hexanocturne@gmail.com>
2026-07-04 10:05:49 +0800

    Add files via upload

--- scenesystem_dll.cs
+++ scenesystem_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-07-02 00:26:33.420883800 UTC
+// 2026-07-04 02:03:49.547220800 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: scenesystem.dll
